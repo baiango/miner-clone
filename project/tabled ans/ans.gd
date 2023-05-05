7f541c76616d3c547f180dd217d95da8a42b1733
baiango <105705580+baiango@users.noreply.github.com>
2023-05-06 02:16:23 +0800

    Checkpoint 1 for greedy meshing & blocks culling

--- project/tabled ans/ans.gd
+++ project/tabled ans/ans.gd
@@ -7,6 +7,11 @@ var dbg := PerformanceInfo.Time
 enum { Dirt, Grass, Stone, Void_grass, Crystal_blue }
 
 
+"""
+_regenerate_fast() stats:
+	Clean up time: 26.132 ms
+	Regenerating time: 258.449 ms or 1014292(100.5x100.5x100.5) blocks/s
+"""
 func _regenerate_fast(dimension: Vector3i) -> void:
 	var clean_time := clean()
 
@@ -18,7 +23,7 @@ func _regenerate_fast(dimension: Vector3i) -> void:
 	var rng = RandomNumberGenerator.new()
 	rng.set_seed(1023)
 
-	for x in range(-dimension.x, dimension.x):
+	for x in dimension.x:
 		for y in dimension.y:
 			for z in dimension.z:
 				var val = noi.get_noise_3d(x, y, z)
@@ -51,31 +56,9 @@ func reset() -> void:
 		if dbg >= PerformanceInfo.Info:
 			print_debug("Current Gridmap hash is not 2170478937 but ", get_hash(), ".\n\t",
 					"Regenerated with _regenerate_fast().")
-		_regenerate_fast(Vector3i(32, 32, 4))
+		_regenerate_fast(Vector3i(64, 64, 64))
 	elif dbg >= PerformanceInfo.Info:
 		print_debug("Gridmap hash is 2170478937, not regenerating.")
-
-
-"""
-# To keep performace in check.
-# It should be over 262144 or 64 cubed (64x64x64) blocks per second on i5-9300H.
-# I am targeting generate 64 cubed blocks in 1.5 seconds on i3-6100U,
-# which has 55% performace of i5-9300H.
-_regenerate_fast() stats:
-	Clean up time: 0.019 ms
-	Regenerating time: 2170.06 ms or 483201(78.5x78.5x78.5) blocks/s
-_regenerate_with_array() stats:
-	Clean up time: 255.129 ms
-	Regenerating time: 2577.446 ms or 406827(74.1x74.1x74.1) blocks/s
-"""
-func benchmark() -> void:
-	clear()
-
-	var dimension := Vector3i(64, 64, 64)
-#	dimension = Vector3i(32, 32, 4) # default dimension
-
-	_regenerate_fast(dimension)
-	_regenerate_with_array(dimension)
 
 
 func get_hash() -> int:
@@ -105,60 +88,7 @@ func cbrt(num: float) -> float: return pow(num, 1.0/3.0)
 
 
 func _ready() -> void: # "Scene -> Reload Saved Scene" to see the changes!
-	if not Engine.is_editor_hint():
-		pass
-#		benchmark()
-
 	reset()
-
-
-# non-production ready
-func _regenerate_with_array(dimension: Vector3i) -> void:
-	var clean_time := clean()
-
-	var start := Time.get_ticks_usec()
-
-	var noi = FastNoiseLite.new()
-	noi.set_frequency(0.03)
-
-	var rng = RandomNumberGenerator.new()
-	rng.set_seed(1023)
-
-	var blk_id_arr := []
-	blk_id_arr.resize(dimension.x * 2)
-	for x in dimension.x * 2:
-		blk_id_arr[x] = []
-		blk_id_arr[x].resize(dimension.y)
-		for y in dimension.y:
-			blk_id_arr[x][y] = []
-			blk_id_arr[x][y].resize(dimension.z)
-
-	for x in range(-dimension.x, dimension.x):
-		for y in dimension.y:
-			for z in dimension.z:
-				var val = noi.get_noise_3d(x, y, z)
-				var rnd_deep := rng.randi()
-
-				if val > -0.3:
-					if y >= (rnd_deep & 1) + 5:
-						blk_id_arr[x + dimension.x][y][z] = Stone
-					elif y >= (rnd_deep & 3) + 1:
-						blk_id_arr[x + dimension.x][y][z] = Dirt
-					elif y >= 0:
-						blk_id_arr[x + dimension.x][y][z] = Grass
-				else:
-					blk_id_arr[x + dimension.x][y][z] = -1
-
-	for x in range(-dimension.x, dimension.x):
-		for y in dimension.y:
-			for z in dimension.z:
-				set_cell_item(Vector3i(x, ~y, z), blk_id_arr[x + dimension.x][y][z])
-
-	if dbg >= PerformanceInfo.Time:
-		var block_sum := dimension.x * dimension.y * dimension.z
-		var regenerating_time := (Time.get_ticks_usec() - start) / 1000.0
-		var bps := floori(block_sum / ((Time.get_ticks_usec() - start) / 1_000_000.0))
-		prt_perf_stat("_regenerate_with_array()", clean_time, regenerating_time, bps)
 
 
 func _on_tree_exiting():
