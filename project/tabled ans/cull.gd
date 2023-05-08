4b89c33c1b1cc33c9d8eb8eb284e52437dd050d7
baiango <105705580+baiango@users.noreply.github.com>
2023-05-09 05:11:25 +0800

    Cp 6: Fixed missing index for right_block

--- project/tabled ans/cull.gd
+++ project/tabled ans/cull.gd
@@ -1,26 +1,27 @@
 @tool
 extends MeshInstance3D
 
+const Vec3_ZERO = Vector3.ZERO
+const Vec3_ONE = Vector3.ONE
+const Vec3_RIGHT = Vector3.RIGHT
+const Vec3_RIGHT_BACK = Vector3.RIGHT + Vector3.BACK
+const Vec3_UP = Vector3.UP
+const Vec3_UP_RIGHT = Vector3.UP + Vector3.RIGHT
+const Vec3_UP_BACK = Vector3.UP + Vector3.BACK
+const Vec3_BACK = Vector3.BACK
+
 enum PerformanceInfo { None, Time, Info, Verbose }
 var dbg := PerformanceInfo.Time
 
 const Air := -1
 enum { Grass, Dirt, Stone, Void_grass, Crystal_blue }
 
-const TOP_FACE    := [Vector3(0, 1, 0), Vector3(1, 1, 0), Vector3.ONE     , Vector3(0, 1, 1)]
-const BOTTOM_FACE := [Vector3(0, 0, 1), Vector3(1, 0, 1), Vector3(1, 0, 0), Vector3.ZERO    ]
-const LEFT_FACE   := [Vector3(0, 0, 1), Vector3.ZERO    , Vector3(0, 1, 0), Vector3(0, 1, 1)]
-const RIGHT_FACE  := [Vector3(1, 0, 0), Vector3(1, 0, 1), Vector3.ONE     , Vector3(1, 1, 0)]
-const FRONT_FACE  := [Vector3(1, 0, 1), Vector3(0, 0, 1), Vector3(0, 1, 1), Vector3.ONE     ]
-const BACK_FACE   := [Vector3.ZERO    , Vector3(1, 0, 0), Vector3(1, 1, 0), Vector3(0, 1, 0)]
-const ALL_FACE    := [
-		Vector3(0, 1, 0), Vector3(1, 1, 0), Vector3.ONE     , Vector3(0, 1, 1),
-		Vector3(0, 0, 1), Vector3(1, 0, 1), Vector3(1, 0, 0), Vector3.ZERO    ,
-		Vector3(0, 0, 1), Vector3.ZERO    , Vector3(0, 1, 0), Vector3(0, 1, 1),
-		Vector3(1, 0, 0), Vector3(1, 0, 1), Vector3.ONE     , Vector3(1, 1, 0),
-		Vector3(1, 0, 1), Vector3(0, 0, 1), Vector3(0, 1, 1), Vector3.ONE     ,
-		Vector3.ZERO    , Vector3(1, 0, 0), Vector3(1, 1, 0), Vector3(0, 1, 0),
-]
+const TOP_FACE    := [Vec3_UP        , Vec3_UP_RIGHT  , Vec3_ONE     , Vec3_UP_BACK ]
+const BOTTOM_FACE := [Vec3_BACK      , Vec3_RIGHT_BACK, Vec3_RIGHT   , Vec3_ZERO    ]
+const LEFT_FACE   := [Vec3_BACK      , Vec3_ZERO      , Vec3_UP      , Vec3_UP_BACK ]
+const RIGHT_FACE  := [Vec3_RIGHT     , Vec3_RIGHT_BACK, Vec3_ONE     , Vec3_UP_RIGHT]
+const FRONT_FACE  := [Vec3_RIGHT_BACK, Vec3_BACK      , Vec3_UP_BACK , Vec3_ONE     ]
+const BACK_FACE   := [Vec3_ZERO      , Vec3_RIGHT     , Vec3_UP_RIGHT, Vec3_UP      ]
 
 
 const FACE_INDEX  := [0, 1, 2, 0, 2, 3]
@@ -71,9 +72,9 @@ func _get_face_uv(BLOCK_ID: int) -> PackedVector2Array:
 	return result
 
 
-enum { Left, Right, Top, Bottom, Back, Front, All }
+enum { Left, Right, Top, Bottom, Back, Front }
 func _generate_face(i: int, x: int, y: int, z: int) -> PackedVector3Array:
-	var Faces := [LEFT_FACE, RIGHT_FACE, TOP_FACE, BOTTOM_FACE, BACK_FACE, FRONT_FACE, ALL_FACE]
+	var Faces := [LEFT_FACE, RIGHT_FACE, TOP_FACE, BOTTOM_FACE, BACK_FACE, FRONT_FACE]
 	return [	Faces[i][0] + Vector3(x, y, z),
 				Faces[i][1] + Vector3(x, y, z),
 				Faces[i][2] + Vector3(x, y, z),
@@ -81,19 +82,20 @@ func _generate_face(i: int, x: int, y: int, z: int) -> PackedVector3Array:
 
 
 """
+Test size: 64 cubed
 # from ans.gd
 _regenerate() stats:
-	Clean up time: 0.224 ms
-	Regenerating time: 316.456 ms or 828371(93.9x93.9x93.9) blocks/s
+	Clean up time: 1.738 ms
+	Regenerating time: 317.534 ms or 825559(93.8x93.8x93.8) blocks/s
 
-# Cull non-edge blocks only.
+# Full Culling single sided
 _ready() stats:
-	Regenerating time: 71.132 ms or 460664(77.2x77.2x77.2) blocks/s
+	Regenerating time: 826.482 ms or 317180(68.2x68.2x68.2) blocks/s
 
-# It speed up by x4.44.
-# But it reduced the efficiency by 44%.
+# It slowed down by x2.6.
 """
 func _ready() -> void:
+#	return
 	var start := Time.get_ticks_usec()
 	# Step 1, generate block id.
 	var dimension := Vector3i(32, 32, 32)
@@ -104,23 +106,28 @@ func _ready() -> void:
 	var rng = RandomNumberGenerator.new()
 	rng.set_seed(1023)
 
+	var padding = 1
+	var padded_dimension = dimension + Vector3i.ONE * padding * 2
 	var blk_id_arr := []
-	blk_id_arr.resize(dimension.x)
-	for x in dimension.x:
+	blk_id_arr.resize(padded_dimension.x)
+	for x in padded_dimension.x:
 		blk_id_arr[x] = []
-		blk_id_arr[x].resize(dimension.y)
-		for y in dimension.y:
+		blk_id_arr[x].resize(padded_dimension.y)
+		for y in padded_dimension.y:
 			blk_id_arr[x][y] = []
-			blk_id_arr[x][y].resize(dimension.z)
+			blk_id_arr[x][y].resize(padded_dimension.z)
 
-	for x in dimension.x:
-		for y in dimension.y:
-			for z in dimension.z:
-				var val = noi.get_noise_3d(x, y, z)
+	for x in padded_dimension.x:
+		for y in padded_dimension.y:
+			for z in padded_dimension.z:
+				blk_id_arr[x][y][z] = Air
+
+	for x in range(padding, dimension.x + 1):
+		for y in range(padding, dimension.y + 1):
+			for z in range(padding, dimension.z + 1):
+				var val = noi.get_noise_3d(x - padding, y - padding, z - padding)
 				var rnd_deep := rng.randi()
 
-				# since modulo "%" use "a-(a//b)*b" which is slow. I use bitwise AND "&".
-				# bitwise AND "&" only work for numbers that are in power of 2 minus 1. (2**n - 1)
 				var blk_id := -1
 				if val > -0.3:
 					if y >= (rnd_deep & 1) + 5:
@@ -132,95 +139,66 @@ func _ready() -> void:
 				blk_id_arr[x][y][z] = blk_id
 
 	# Step 2, generate meshes.
-	var arr_mesh := ArrayMesh.new()
-	var face_arr := []
-	var face_index := []
-	var face_uv := []
+	var face_arr := PackedVector3Array()
+	var face_index := PackedInt32Array()
+	var face_uv := PackedVector2Array()
 
-	# Non-edge
 	var face_count := 0
-	for x in range(1, dimension.x - 1):
-		for y in range(1, dimension.y - 1):
-			for z in range(1, dimension.z - 1):
-				var in_place := blk_id_arr[x][y][z] as int
+	for ix in range(padding, dimension.x + 1):
+		for iy in range(padding, dimension.y + 1):
+			for iz in range(padding, dimension.z + 1):
+				var in_place := blk_id_arr[ix][iy][iz] as int
 				if in_place == Air:
 					continue
 
-				var left_block := blk_id_arr[x-1][y][z] as int
-				var right_block := blk_id_arr[x+1][y][z] as int
-				var top_block := blk_id_arr[x][y-1][z] as int
-				var bottom_block := blk_id_arr[x][y+1][z] as int
-				var back_block := blk_id_arr[x][y][z-1] as int
-				var front_block := blk_id_arr[x][y][z+1] as int
+				var dbg_do_all_face := false
+#				dbg_do_all_face = not dbg_do_all_face
 
-				if left_block == Air:
+				var left_block := blk_id_arr[ix-1][iy][iz] as int
+				var right_block := blk_id_arr[ix+1][iy][iz] as int
+				var top_block := blk_id_arr[ix][iy-1][iz] as int
+				var bottom_block := blk_id_arr[ix][iy+1][iz] as int
+				var back_block := blk_id_arr[ix][iy][iz-1] as int
+				var front_block := blk_id_arr[ix][iy][iz+1] as int
+
+				var x := ix - padding as int
+				var y := iy - padding as int
+				var z := iz - padding as int
+
+				if left_block == Air or dbg_do_all_face:
 					face_arr.append_array(_generate_face(Left, x, ~y, z))
-					face_uv.append_array(_get_face_uv(blk_id_arr[x][y][z]))
+					face_uv.append_array(_get_face_uv(blk_id_arr[ix][iy][iz]))
 					face_count += 1
-				if right_block == Air:
+				if right_block == Air or dbg_do_all_face:
 					face_arr.append_array(_generate_face(Right, x, ~y, z))
-					face_uv.append_array(_get_face_uv(blk_id_arr[x][y][z]))
-				if top_block == Air:
+					face_uv.append_array(_get_face_uv(blk_id_arr[ix][iy][iz]))
+					face_count += 1
+				if top_block == Air or dbg_do_all_face:
 					face_arr.append_array(_generate_face(Top, x, ~y, z))
-					face_uv.append_array(_get_face_uv(blk_id_arr[x][y][z]))
+					face_uv.append_array(_get_face_uv(blk_id_arr[ix][iy][iz]))
 					face_count += 1
-				if bottom_block == Air:
+				if bottom_block == Air or dbg_do_all_face:
 					face_arr.append_array(_generate_face(Bottom, x, ~y, z))
-					face_uv.append_array(_get_face_uv(blk_id_arr[x][y][z]))
+					face_uv.append_array(_get_face_uv(blk_id_arr[ix][iy][iz]))
 					face_count += 1
-				if back_block == Air:
+				if back_block == Air or dbg_do_all_face:
 					face_arr.append_array(_generate_face(Back, x, ~y, z))
-					face_uv.append_array(_get_face_uv(blk_id_arr[x][y][z]))
+					face_uv.append_array(_get_face_uv(blk_id_arr[ix][iy][iz]))
 					face_count += 1
-				if front_block == Air:
+				if front_block == Air or dbg_do_all_face:
 					face_arr.append_array(_generate_face(Front, x, ~y, z))
-					face_uv.append_array(_get_face_uv(blk_id_arr[x][y][z]))
+					face_uv.append_array(_get_face_uv(blk_id_arr[ix][iy][iz]))
 					face_count += 1
-
-	# Edge only
-	$"../../GridMap".clear()
-	# x or left direction
-	for x in [0, dimension.x - 1]:
-		for y in dimension.y:
-			for z in dimension.z:
-				# Stupid meshlib!
-				var id := blk_id_arr[x][y][z] as int
-				if id == 0:
-					id = Dirt
-				elif id == 1:
-					id = Grass
-				$"../../GridMap".set_cell_item(Vector3i(x, ~y, z), id)
-
-	# y or down direction
-	for x in range(1, dimension.x - 1):
-		for y in [0, dimension.y - 1]:
-			for z in range(1, dimension.z - 1):
-				var id := blk_id_arr[x][y][z] as int
-				if id == 0:
-					id = Dirt
-				elif id == 1:
-					id = Grass
-				$"../../GridMap".set_cell_item(Vector3i(x, ~y, z), id)
-
-	# z or front direction or where kira is facing in.
-	for x in range(1, dimension.x - 1):
-		for y in range(0, dimension.y - 1):
-			for z in [0, dimension.z - 1]:
-				var id := blk_id_arr[x][y][z] as int
-				if id == 0:
-					id = Dirt
-				elif id == 1:
-					id = Grass
-				$"../../GridMap".set_cell_item(Vector3i(x, ~y, z), id)
-
 
 	face_index = _add_face_index(face_count)
 
 	var mesh_data := []
 	mesh_data.resize(ArrayMesh.ARRAY_MAX)
-	mesh_data[ArrayMesh.ARRAY_VERTEX] = PackedVector3Array(face_arr)
-	mesh_data[ArrayMesh.ARRAY_INDEX]  = PackedInt32Array(face_index)
-	mesh_data[ArrayMesh.ARRAY_TEX_UV] = PackedVector2Array(face_uv)
+	mesh_data[ArrayMesh.ARRAY_VERTEX] = face_arr
+	mesh_data[ArrayMesh.ARRAY_INDEX] = face_index
+	mesh_data[ArrayMesh.ARRAY_TEX_UV] = face_uv
+
+	var arr_mesh := ArrayMesh.new()
 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
 	mesh = arr_mesh
 
