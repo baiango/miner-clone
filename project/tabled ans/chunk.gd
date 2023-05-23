ca5a23f7a871afc7fd3441c41946905a9dfbb23c
baiango <105705580+baiango@users.noreply.github.com>
2023-05-23 18:49:59 +0800

    Cp 16: Rectangle culling bug fix
    Now it supports rectangle chunks! But there were index bugs when Kira mined the block outside the chunk. It will crash the game. A new bug discovered! And it's challenging to detect due to Godot supporting the awful negative index. And I don't know how to turn on the warning for the negative index or disable it. Sometimes it's out of index. This bug is much easier to solve with a 3d array. But it's not easy to make 3d arrays in Godot. So I sticking with the 1d arrays.

--- project/tabled ans/chunk.gd
+++ project/tabled ans/chunk.gd
@@ -9,7 +9,7 @@ enum { Dirt, Grass, Stone, Void_grass, Crystal_blue, Air=254, Reset=255 }
 var body = RID()
 
 
-var dimension := Vector3i(64, 128, 64)
+var dimension := Vector3i(64, 128, 32)
 var row := dimension.x
 var col := dimension.y
 var cll := dimension.z # cells
@@ -23,7 +23,7 @@ func _ready() -> void:
 	_regenerate()
 
 
-func _regenerate(pos: Vector3i = Vector3i.ZERO) -> void:
+func _regenerate(pos: Vector3i = Vector3i(position.x, position.y + col, position.z)) -> void:
 	var clean_time := clean()
 	var start: float = Time.get_ticks_usec()
 
@@ -74,56 +74,27 @@ func _regenerate(pos: Vector3i = Vector3i.ZERO) -> void:
 				if y+1 < col: set_cell_item(v3 + Vector3.UP,    blk_id_arr[x   + ((y+1)*row) + (z*row*col)]) # does this cause the top block not showing? fixed it by using col instead of row
 				if z+1 < cll: set_cell_item(v3 + Vector3.BACK,  blk_id_arr[x   + (y*row)     + ((z+1)*row*col)])
 
-
 	# Edge
-#	for i in row: # 282.315 ms
-#		for j in cll:
-#			# This won't work well with non-cube chunk.
-#			var LEFT_BLOCK := blk_id_arr[i*row + (j*row*col)] # LEFT_BLOCK as in Vector3.LEFT
-#			var RIGHT_BLOCK := blk_id_arr[row - 1 + (i*row) + (j*row*col)]
-#			var UP_BLOCK := blk_id_arr[i + ((col-1)*row) + (j*row*col)]
-#			var DOWN_BLOCK := blk_id_arr[i + (j*row*col)] # Why the row has to be 1? # Fixed now
-#			var FORWARD_BLOCK := blk_id_arr[i + (j*row)] # Note! FORWARD as is Vector3.FORWARD
-#			var BACK_BLOCK := blk_id_arr[i + (j*row) + ((cll - 1)*row*col)]
-#			if LEFT_BLOCK != Air:    set_cell_item(Vector3(0, i, j), LEFT_BLOCK)
-#			if RIGHT_BLOCK != Air:   set_cell_item(Vector3(row - 1, i, j), RIGHT_BLOCK)
-#			if UP_BLOCK != Air:      set_cell_item(Vector3(i, col - 1, j), UP_BLOCK)
-#			if DOWN_BLOCK != Air:    set_cell_item(Vector3(i, 0, j), DOWN_BLOCK)
-#			if FORWARD_BLOCK != Air: set_cell_item(Vector3(i, j, 0), FORWARD_BLOCK)
-#			if BACK_BLOCK != Air:    set_cell_item(Vector3(i, j, cll - 1), BACK_BLOCK)
-
-	# New one, this might not work well with rectangle chunks. I didn't tested it.
-	# But it solves the height problem. 282.967 ms
 	for i in row:
 		for j in col:
-			var LEFT_BLOCK := blk_id_arr[j*row + (i*row*col)]  # LEFT_BLOCK as in Vector3.LEFT
-			var RIGHT_BLOCK := blk_id_arr[cll - 1 + (j*row) + (i*row*col)]
 			var FORWARD_BLOCK := blk_id_arr[i + (j*row)] # Note! FORWARD as is Vector3.FORWARD
 			var BACK_BLOCK := blk_id_arr[i + (j*row) + ((cll-1)*row*col)]
-			if LEFT_BLOCK != Air:    set_cell_item(Vector3(0, j, i), LEFT_BLOCK)
-			if RIGHT_BLOCK != Air:   set_cell_item(Vector3(cll - 1, j, i), RIGHT_BLOCK)
 			if FORWARD_BLOCK != Air: set_cell_item(Vector3(i, j, 0), FORWARD_BLOCK)
 			if BACK_BLOCK != Air:    set_cell_item(Vector3(i, j, cll - 1), BACK_BLOCK)
 
-	# ChatGPT, slow down by 20%. And barely improve readability.
-#	for i in row:
-#		for j in col:
-#			var positions = PackedVector3Array([
-#				Vector3(i, j, 0),
-#				Vector3(i, j, cll - 1),
-#				Vector3(0, j, i),
-#				Vector3(cll - 1, j, i)
-#			])
-#			for position in positions:
-#				set_cell_item(position, Stone)
+	for i in cll:
+		for j in col:
+			var LEFT_BLOCK := blk_id_arr[j*row + (i*row*col)]  # LEFT_BLOCK as in Vector3.LEFT
+			var RIGHT_BLOCK := blk_id_arr[row - 1 + (j*row) + (i*row*col)]
+			if LEFT_BLOCK != Air:    set_cell_item(Vector3(0, j, i), LEFT_BLOCK)
+			if RIGHT_BLOCK != Air:   set_cell_item(Vector3(row - 1, j, i), RIGHT_BLOCK)
 
-	# Bottom and top
 	for i in row:
-		for k in cll:
-			var UP_BLOCK := blk_id_arr[i + ((col-1)*row) + (k*row*col)]
-			var DOWN_BLOCK := blk_id_arr[i + (k*row*col)]
-			if UP_BLOCK != Air: set_cell_item(Vector3(i, col - 1, k), UP_BLOCK)
-			if DOWN_BLOCK != Air: set_cell_item(Vector3(i, 0, k), DOWN_BLOCK)
+		for j in cll:
+			var UP_BLOCK := blk_id_arr[i + ((col-1)*row) + (j*row*col)]
+			var DOWN_BLOCK := blk_id_arr[i + (j*row*col)]
+			if UP_BLOCK != Air:      set_cell_item(Vector3(i, col - 1, j), UP_BLOCK)
+			if DOWN_BLOCK != Air:    set_cell_item(Vector3(i, 0, j), DOWN_BLOCK)
 
 	set_cell_time = (Time.get_ticks_usec() - set_cell_time) / 1000.0
 
