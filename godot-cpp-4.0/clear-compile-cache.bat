a5ce8f6056921a366a1845a50583720c71c2f5e1
baiango <105705580+baiango@users.noreply.github.com>
2023-05-28 13:04:17 +0800

    Cp 22: Added C# and removed C++ and custom Godot
    It just takes way more effort to optimize in C++ than in C#. And I am short on development time. So I am using C# for speed instead. You can easily write slow C++ code so it's not even worth developing in it.
    See https://stackoverflow.com/a/37103437 for C# and C++ performance development.

--- godot-cpp-4.0/clear-compile-cache.bat
+++
@@ -1,8 +0,0 @@
-for /r %%f in (*.lib) do del "%%f"
-for /r %%f in (*.obj) do del "%%f"
-for /r %%f in (*.exp) do del "%%f"
-for /r %%f in (*.dll) do del "%%f"
-for /r %%f in (*.dblite) do del "%%f"
-for /r %%f in (*.pyc) do del "%%f"
-rmdir /S /Q gen & ^
-pause
