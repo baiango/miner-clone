4b7e3cb3ce6625e36b598da4b7d86954b3784ae8
baiango <105705580+baiango@users.noreply.github.com>
2023-06-03 18:17:54 +0800

    Cp 27: Fixed the culling bug
    What a relief to fix it!

--- project/tabled ans/exam_bak.gd
+++ project/tabled ans/exam_bak.gd
@@ -37,29 +37,73 @@ func _physics_process(delta: float) -> void:
 	var BACK_FACE    := PackedVector3Array([FORWARD_FACE[1],FORWARD_FACE[0],FORWARD_FACE[3],FORWARD_FACE[2]])
 	BACK_FACE  = vectorized_add_v3([Vector3.BACK,Vector3.BACK,Vector3.BACK,Vector3.BACK], BACK_FACE)
 
+#	for x in row:
+#		for y in col:
+#			for z in cll:
+#				var i := flat_3d_to_1d(x, y, z)
+#				var switches := neighbours_arr[i]
+##				switches = 0
+#				if not x and not y and not z:
+#					print(neighbours_arr[row] & 1)
+#					print(neighbours_arr[row] & 2)
+#					print(neighbours_arr[row] & 4)
+#					print(neighbours_arr[row] & 8)
+#					print(neighbours_arr[row] & 16)
+#					print(neighbours_arr[row] & 32)
+#					print(neighbours_arr[row] & 64)
+#				var tmp_vert := PackedVector3Array([Vector3(x,y,z),Vector3(x,y,z),Vector3(x,y,z),Vector3(x,y,z)])
+##				if switches >= 0b111111: continue
+#				if not switches & Neighbour.Left:    face_vert.append_array(vectorized_add_v3(tmp_vert, LEFT_FACE))
+#				if not switches & Neighbour.Right:   face_vert.append_array(vectorized_add_v3(tmp_vert, RIGHT_FACE))
+#				if not switches & Neighbour.Down:    face_vert.append_array(vectorized_add_v3(tmp_vert, DOWN_FACE))
+#				if not switches & Neighbour.Up:      face_vert.append_array(vectorized_add_v3(tmp_vert, UP_FACE))
+#				if not switches & Neighbour.Forward: face_vert.append_array(vectorized_add_v3(tmp_vert, FORWARD_FACE))
+#				if not switches & Neighbour.Back:    face_vert.append_array(vectorized_add_v3(tmp_vert, BACK_FACE))
+
 	for x in row:
 		for y in col:
 			for z in cll:
 				var i := flat_3d_to_1d(x, y, z)
 				var tmp_vert := PackedVector3Array([Vector3(x,y,z),Vector3(x,y,z),Vector3(x,y,z),Vector3(x,y,z)])
+#				if x-1 > 0 and not blk_id_arr[flat_3d_to_1d(x-1, y, z)]:
+#					face_vert.append_array(vectorized_add_v3(tmp_vert, LEFT_FACE))
+#				if x+1 < row and not blk_id_arr[flat_3d_to_1d(x+1, y, z)]:
+#					face_vert.append_array(vectorized_add_v3(tmp_vert, RIGHT_FACE))
+#				if y-1 > 0 and not blk_id_arr[flat_3d_to_1d(x, y-1, z)]:
+#					face_vert.append_array(vectorized_add_v3(tmp_vert, DOWN_FACE))
+#				if y+1 < col and not blk_id_arr[flat_3d_to_1d(x, y+1, z)]:
+#					face_vert.append_array(vectorized_add_v3(tmp_vert, UP_FACE))
+#				if z-1 > 0 and not blk_id_arr[flat_3d_to_1d(x, y, z-1)]:
+#					face_vert.append_array(vectorized_add_v3(tmp_vert, FORWARD_FACE))
+#				if z+1 < cll and not blk_id_arr[flat_3d_to_1d(x, y, z+1)]:
+#					face_vert.append_array(vectorized_add_v3(tmp_vert, BACK_FACE))
 				if blk_id_arr[i] != Air:
-					# chatGPT made it less cluttered
-#					if x-1 >= 0: # first is messy, then chatGPT make it short in python code
-#						if blk_id_arr[i-1] == Air:
-#							face_vert.append_array(vectorized_add_v3(tmp_vert, LEFT_FACE))
-#					else:
+					if i-1 > 0 and blk_id_arr[i-1] == Air:
+						face_vert.append_array(vectorized_add_v3(tmp_vert, LEFT_FACE))
+					if i+1 < dimension_sum and blk_id_arr[i+1] == Air:
+						face_vert.append_array(vectorized_add_v3(tmp_vert, RIGHT_FACE))
+					if i-row > 0 and blk_id_arr[i-row] == Air:
+						face_vert.append_array(vectorized_add_v3(tmp_vert, DOWN_FACE))
+					if i+row < dimension_sum and blk_id_arr[i+row] == Air:
+						face_vert.append_array(vectorized_add_v3(tmp_vert, UP_FACE))
+					if i-(row*col) > 0 and blk_id_arr[i-(row*col)] == Air:
+						face_vert.append_array(vectorized_add_v3(tmp_vert, FORWARD_FACE))
+					if i+(row*col) < dimension_sum and blk_id_arr[i+(row*col)] == Air:
+						face_vert.append_array(vectorized_add_v3(tmp_vert, BACK_FACE))
+#				if blk_id_arr[i] == Air:
+#					if i-1 > 0 and blk_id_arr[i-1] != Air:
 #						face_vert.append_array(vectorized_add_v3(tmp_vert, LEFT_FACE))
+#					if i+1 < dimension_sum and blk_id_arr[i+1] != Air:
+#						face_vert.append_array(vectorized_add_v3(tmp_vert, RIGHT_FACE))
+#					if i-row > 0 and blk_id_arr[i-row] != Air:
+#						face_vert.append_array(vectorized_add_v3(tmp_vert, DOWN_FACE))
+#					if i+row < dimension_sum and blk_id_arr[i+row] != Air:
+#						face_vert.append_array(vectorized_add_v3(tmp_vert, UP_FACE))
+#					if i-(row*col) > 0 and blk_id_arr[i-(row*col)] != Air:
+#						face_vert.append_array(vectorized_add_v3(tmp_vert, FORWARD_FACE))
+#					if i+(row*col) < dimension_sum and blk_id_arr[i+(row*col)] != Air:
+#						face_vert.append_array(vectorized_add_v3(tmp_vert, BACK_FACE))
 
-					# Second, chatGPT output this code in python
-#					face_vert.append_array(vectorized_add_v3(tmp_vert, LEFT_FACE)) if x-1 < 0 or blk_id_arr[i-1] == Air else None
-
-					# Third, I translated this code to GDScript
-					face_vert.append_array(vectorized_add_v3(tmp_vert, LEFT_FACE)) if x-1 < 0 or blk_id_arr[i-1] == Air else null
-					face_vert.append_array(vectorized_add_v3(tmp_vert, RIGHT_FACE)) if x+1 >= row or blk_id_arr[i+1] == Air else null
-					face_vert.append_array(vectorized_add_v3(tmp_vert, DOWN_FACE)) if y-1 < 0 or blk_id_arr[i-row] == Air else null
-					face_vert.append_array(vectorized_add_v3(tmp_vert, UP_FACE)) if y+1 >= col or blk_id_arr[i+row] == Air else null
-					face_vert.append_array(vectorized_add_v3(tmp_vert, FORWARD_FACE)) if z-1 < 0 or blk_id_arr[i-(row*col)] == Air else null
-					face_vert.append_array(vectorized_add_v3(tmp_vert, BACK_FACE)) if z+1 >= cll or blk_id_arr[i+(row*col)] == Air else null
 
 	for iv in range(0, face_vert.size(), 4):
 		face_index.append_array([0+iv, 1+iv, 2+iv, 2+iv, 1+iv, 3+iv])
@@ -68,7 +112,14 @@ func _physics_process(delta: float) -> void:
 		for y in col:
 			for z in cll:
 				var i := flat_3d_to_1d(x, y, z)
-				printraw(neighbours_arr[i])
+				printraw(blk_id_arr[i])
+
+#	print(face_vert)
+#	print(face_index)
+#	print(neighbours_arr)
+#	print(neighbours_arr[0])
+#	print(face_vert.size())
+#	print(face_index.size())
 
 	if num_to_show >= 0 and num_to_show <= face_index.size():
 		face_index.resize(num_to_show)
@@ -126,10 +177,9 @@ func _generate_block_id(pos: Vector3 = position) -> PackedByteArray:
 					elif y > 0:
 						blk_id = Stone
 
-#				blk_id = Dirt # Test the culling
 				ret[flat_3d_to_1d(x, y, z)] = blk_id
 
-	if ret.has(Error): push_error("Found unused in the block id array ", ret.find(Error))
+	if ret.find(Error) != -1: push_error("Found unused in the block id array ", ret.find(Error))
 	return ret
 
 func _generate_neighbours_arr(blk_id_arr: PackedByteArray) -> PackedByteArray:
